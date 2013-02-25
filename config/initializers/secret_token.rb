43cf971b4b64f7d2bdd68077c7f65a988d48ccc6
ror <ror@ror-VirtualBox.(none)>
2013-02-25 09:29:56 +0100

    User Authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharebox::Application.config.secret_token = 'b629153728a2e467cd257b2ba718d05cdd60574aac65bc9fa7c01e591df9a787c0e825fcda543e6bfa2e3ee721dec16c3d7e0641251da578d35d02246a28708f'
